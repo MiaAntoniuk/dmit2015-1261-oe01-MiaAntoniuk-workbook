0fe6383d40bdd18ef426deaddaf2dbd8fa7f5068
Mia Antoniuk <ray.seven132@gmail.com>
2026-09-21 11:53:11 -0600

    changed something I messed up with

---
+++ Lesson-4A-Student-Practice-DMIT2015/src/main/java/dmit2015/view/GreetingBean.java
@@ -0,0 +1,28 @@
+package dmit2015.view;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class GreetingBean {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getGreetingMessage() {
+
+        if (firstName == null || firstName.isBlank()) {
+            return "";
+        }
+
+        return "Welcome " + firstName + " to DMIT2015!";
+    }
+}
